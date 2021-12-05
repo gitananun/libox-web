f3f5007e5056f62941c14353281cee44e2af3773
gitananun <tigran.muradyan140201@gmaail.com>
2021-12-05 14:50:17 +0400

    Not Found Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import Index from 'views/Index';
 import 'assets/css/main.scss';
+import { Routes, Route } from 'react-router-dom';
+import NotFound from 'views/NotFound';
 
 const App = () => {
-  return <Index />;
+  return (
+    <Routes>
+      <Route path='/' element={<Index />} />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
+  );
 };
 
 export default App;
