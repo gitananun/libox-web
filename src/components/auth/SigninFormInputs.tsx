c12424d12e4e88656367d4346df8a15019dffde3
gitananun <tigran.muradyan140201@gmaail.com>
2021-12-07 16:44:59 +0400

    Forgot Password Link Signin

--- src/components/auth/SigninFormInputs.tsx
+++ src/components/auth/SigninFormInputs.tsx
@@ -1,10 +1,14 @@
 import FormInput from 'components/form/FormInput';
+import { Link } from 'react-router-dom';
 
 const SigninFormInputs = () => {
   return (
     <>
       <FormInput placeholder='libox@best.com' type='email' label='Email' />
       <FormInput placeholder='password' type='password' label='Password' />
+      <Link to='/' className='w-100 text-end mb-3'>
+        Forgot Password?
+      </Link>
     </>
   );
 };
