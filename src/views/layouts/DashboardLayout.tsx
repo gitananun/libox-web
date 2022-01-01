2d5fb6638fbcdd1e7af6749d68aae8f0b778e867
gitananun <tigran.muradyan140201@gmaail.com>
2022-01-01 14:53:52 +0400

    removed scroll top

--- src/views/layouts/DashboardLayout.tsx
+++ src/views/layouts/DashboardLayout.tsx
@@ -1,12 +1,10 @@
 import DashboardHeader from 'components/layouts/DashboardHeader';
 import HeaderWrapper from 'components/layouts/HeaderWrapper';
-import ScrollTopBtn from 'components/layouts/ScrollTopBtn';
 import Navbar from '../../components/layouts/Navbar';
 
 const DashboardLayout = (props: any) => {
   return (
     <div className='dashboard'>
-      <ScrollTopBtn />
       <HeaderWrapper>
         <Navbar />
       </HeaderWrapper>
